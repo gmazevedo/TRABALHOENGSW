f4b3273e39b99502346ab791cf898eb4e7f8a998
Gessica <gessica.azevedo@aprix.com.br>
2022-08-14 19:34:53 -0300

    visual changes

--- front-end/src/app/components/login/login.component.ts
+++ front-end/src/app/components/login/login.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
@@ -7,9 +15,13 @@ import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from '../../services/auth.service';
 
-import { PRIMARY_OUTLET, Router, UrlSegment, UrlSegmentGroup } from '@angular/router';
+import {
+  PRIMARY_OUTLET,
+  Router,
+  UrlSegment,
+  UrlSegmentGroup,
+} from '@angular/router';
 // import { AngularMaterialModule } from '../../core/angular-material.module';
-
 
 export interface DialogData {
   animal: 'panda' | 'unicorn' | 'lion';
@@ -22,34 +34,44 @@ export interface DialogData {
 export class LoginComponent implements OnInit {
   @ViewChild('errorDialog') errorDialog: TemplateRef<any>;
   public form: FormGroup = this.fb.group({
-    user_number: this.fb.control('', [Validators.required, Validators.pattern("^[0-9]*$"), Validators.maxLength(8), Validators.minLength(8)]),
+    user_number: this.fb.control('', [
+      Validators.required,
+      Validators.pattern('^[a-z0-9]*$'),
+      Validators.maxLength(8),
+      Validators.minLength(4),
+    ]),
     password: this.fb.control('', [Validators.required]),
   });
 
-  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder, private dialog: MatDialog) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder,
+    private dialog: MatDialog
+  ) {
     const urlTree = this.router.parseUrl(this.router.url);
     const segmentGroup: UrlSegmentGroup = urlTree.root.children[PRIMARY_OUTLET];
     const urlSegments: UrlSegment[] = segmentGroup.segments;
-
-
   }
 
   public async ngOnInit() {
     if (this.authService.isLoggedIn()) {
-      this.router.navigate['']
+      this.router.navigate[''];
     }
   }
 
   public async onClick() {
     try {
-      await this.authService.login(this.form.get('user_number').value, this.form.get('password').value);
+      await this.authService.login(
+        this.form.get('user_number').value,
+        this.form.get('password').value
+      );
     } catch (err) {
-      this.openErrorDialog()
+      this.openErrorDialog();
     }
   }
 
   public openErrorDialog() {
     this.dialog.open(this.errorDialog);
   }
-
 }
